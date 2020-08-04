188ec811d978e5dfb3a834825238f1765a468027
V.S.M <sampath.vithanage1@gmail.com>
2020-08-04 22:40:03 +0300

    outputting widgets conditionally

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import './answer.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key key }) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -13,44 +13,53 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _questions = [
+    {
+      'question': 'What is your favorite color?',
+      'answers': ['red', 'blue', 'green'],
+    },
+    {
+      'question': 'What is your favorite flower?',
+      'answers': ['rose', 'sunflower', 'jasmine'],
+    },
+    {
+      'question': 'What is your favorite fruit?',
+      'answers': ['apple', 'orange', 'banana'],
+    }
+  ];
   void _answer() {
-    // print('Answer 1 for the question is provided!');
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    if (_questionIndex < _questions.length) {
+      print('You have more questions!');
+    } else {
+      print('You are done!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'question': 'What is your favorite color?',
-        'answers': ['red', 'blue', 'green'],
-      },
-      {
-        'question': 'What is your favorite flower?',
-        'answers': ['rose', 'sunflower', 'jasmine'],
-      },
-      {
-        'question': 'What is your favorite fruit?',
-        'answers': ['apple', 'orange', 'banana'],
-      }
-    ];
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Column(children: <Widget>[
-          Question(questions.elementAt(_questionIndex)['question']),
-          ...(questions.elementAt(_questionIndex)['answers'] as List<String>).map(
-            (answer) {
-              return Answer(_answer, answer);
-            }
-          ).toList(),
-        ],)
-      ),
+          appBar: AppBar(
+            title: Text('Welcome to Flutter'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Column(
+                  children: <Widget>[
+                    Question(_questions.elementAt(_questionIndex)['question']),
+                    ...(_questions.elementAt(_questionIndex)['answers']
+                            as List<String>)
+                        .map((answer) {
+                      return Answer(_answer, answer);
+                    }).toList(),
+                  ],
+                )
+              : Center(
+                  child: Text('You are Done!'),
+                )),
     );
   }
 }
