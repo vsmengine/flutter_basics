a0e62ce9519e8e3bfc30266dbfe569e1a2606ebb
V.S.M <sampath.vithanage1@gmail.com>
2020-08-05 18:29:28 +0300

    getters and else if statement

--- lib/alert.dart
+++ lib/alert.dart
@@ -3,10 +3,23 @@ import 'package:flutter/material.dart';
 class Alert extends StatelessWidget {
   final int scoreOutput;
   Alert(this.scoreOutput);
+
+  String get completionMsg{
+    String msgText;
+    if(scoreOutput < 3) {
+      msgText = 'You are failed!';
+    } else if(scoreOutput < 6){
+      msgText = 'You are just passed!';
+    } else {
+      msgText = 'You are genius!';
+    }
+    return msgText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You are Done!' + scoreOutput.toString()),
+      child: Text(completionMsg + '-' + scoreOutput.toString()),
     );
   }
 }
